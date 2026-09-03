7e965dc3b496b9c41ebe37fe2efe6b4ebc50d11a
Rupesh Waghadhare <waghdhare1996@gmail.com>
2026-09-03 07:43:30 +0530

    chore: prepare interactive business profile UI

--- SalesCallTracker/app/build.gradle.kts
+++ SalesCallTracker/app/build.gradle.kts
@@ -1,4 +1,4 @@
-﻿plugins {
+plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.compose.compiler)
   alias(libs.plugins.kotlin.serialization)
@@ -68,7 +68,8 @@ dependencies {
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.compose.material3)
-  // Tooling
+  implementation(libs.androidx.compose.material.icons.extended)
+// Tooling
   debugImplementation(libs.androidx.compose.ui.tooling)
   // Instrumented tests
   androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -90,4 +91,5 @@ dependencies {
   implementation(libs.androidx.navigation3.runtime)
   implementation(libs.androidx.lifecycle.viewmodel.navigation3)
 }
+
 
